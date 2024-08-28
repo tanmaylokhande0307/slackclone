9b735ec329b301ee59f6213fb6f5fbd6a18bc4a7
debian <tl@zasmlabs.com>
2024-08-29 02:48:02 +0530

    sign in gorm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
 import { AuthScreen } from "@/features/auth/components/auth-screen";
-import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div className="text-rose-500">
-      <AuthScreen />
-    </div>
-  );
+  return <AuthScreen />;
 }
-                                    
---
+++ src/features/auth/components/sign-in-card.tsx
@@ -0,0 +1,23 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+
+export const SignInCard = () => {
+    return (
+        <Card className="w-full h-full p-8">
+            <CardHeader className="px-0 pt-0">
+                <CardTitle>
+
+                Login to continue
+                </CardTitle>
+            </CardHeader>
+            <CardDescription>
+                Use your email or another service to continue
+            </CardDescription>
+            <CardContent className="space-y-5 px-0 pb-0">
+                <form className="space-y-2.5">
+                    <Input disabled={false} value="" onChange={()=>{}} required type="email" placeholder="Email"/>
+                </form>
+            </CardContent>
+        </Card>
+    )
+}
